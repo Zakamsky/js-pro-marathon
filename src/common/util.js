04f0e5949988f09a43220139f5a3bceeccfefaf6
Zakamsky <alex.zakamsky@gmail.com>
2021-08-14 18:04:26 +0200

    hw7 result

--- src/common/util.js
+++ src/common/util.js
@@ -18,3 +18,7 @@ export function animateEx(dx, startTime, currentTime, speed, looped = false) {
 
   return { offset: dx * time, progress: time };
 }
+
+export function getTime(date) {
+  return new Date(date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'/*, second:'2-digit'*/})
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import { io } from 'socket.io-client';
 import './index.scss';
 import ClientGame from './client/ClientGame';
+import {getTime} from "./common/util";
 
 window.addEventListener('load', () => {
+
   const greeting = document.querySelector('.start-game');
   const form = document.getElementById('nameForm');
   const name = form.querySelector('.input');
@@ -13,6 +16,9 @@ window.addEventListener('load', () => {
   const chatWrap = document.querySelector('.chat-wrap');
   const chatForm = chatWrap.querySelector('#form');
   const chatInput = chatForm.querySelector('#input');
+  const messageBlock = chatWrap.querySelector('.message');
+
+  let socket = null;
 
   const submitName = (evt) => {
     evt.preventDefault();
@@ -32,9 +38,41 @@ window.addEventListener('load', () => {
       playerName: name.value,
     });
 
+
+    const myName = name.value;
+
+    socket = io('https://jsprochat.herokuapp.com/');
+    socket.emit('start', name.value);
+
     chatWrap.style.display = 'block';
     form.removeEventListener('submit', submitName);
     greeting.remove();
+
+
+    socket.on('chat online', (data) => {
+      console.log('### io chat online data: ', data);
+
+      messageBlock.insertAdjacentHTML('afterbegin', `<br><p><b>${getTime(data.time)}</b> - Всего онлайн: ${data.online}</p><br>`);
+    })
+
+    socket.on('chat connection', (data) => {
+      console.log('### io connection data', data);
+      messageBlock.insertAdjacentHTML('afterbegin', `<p><b>${getTime(data.time)}</b> - ${data.msg}</p>`);
+    })
+
+    socket.on('chat message', (data) => {
+      console.log('### io message data', data);
+      const mssgClass = myName === data.name ? 'accent' : '';
+      const mssgHtml = `<p class="${mssgClass}"><b>${getTime(data.time)}</b>/<i>${data.name}</i> - ${data.msg}</p>`;
+      messageBlock.insertAdjacentHTML('afterbegin', mssgHtml);
+    })
+
+    socket.on('chat disconnect', (data) => {
+      console.log('### io message data', data);
+
+      messageBlock.insertAdjacentHTML('afterbegin', `<p><b>${getTime(data.time)}</b> - ${data.msg}</p>`);
+    })
+
   };
 
   form.addEventListener('submit', submitName);
@@ -43,9 +81,11 @@ window.addEventListener('load', () => {
     evt.preventDefault();
 
     if (chatInput.value) {
-      console.log('### chatInput.value: ', chatInput.value);
-
+      console.log( '### chatInput.value: ', chatInput.value );
+      socket.emit( 'chat message', chatInput.value );
       chatInput.value = '';
     }
   });
+
+
 });
