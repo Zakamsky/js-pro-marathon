22ee6370001759b195d4b34af31f0da189518d16
Zakamsky <alex.zakamsky@gmail.com>
2021-08-14 15:46:59 +0200

    add chat

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ window.addEventListener('load', () => {
   const greeting = document.querySelector('.start-game');
   const form = document.getElementById('nameForm');
   const name = form.querySelector('.input');
+
   const warning = {};
   warning.long = form.querySelector('.warning--long');
   warning.short = form.querySelector('.warning--short');
+
+  const chatWrap = document.querySelector('.chat-wrap');
+  const chatForm = chatWrap.querySelector('#form');
+  const chatInput = chatForm.querySelector('#input');
+
 
   const submitName = (evt) => {
     evt.preventDefault();
@@ -22,28 +28,27 @@ window.addEventListener('load', () => {
       return;
     }
 
-    // greeting.parentNode.removeChild(greeting);
-    ClientGame.init({ tagID: 'game', playerName: name.value });
+    ClientGame.init({
+      tagID: 'game',
+      playerName: name.value
+    });
+
+    chatWrap.style.display = 'block';
     form.removeEventListener('submit', submitName);
     greeting.remove();
   };
-  /* form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    Object.values(warning).map((item) => item.classList.remove('warning--show'));
-
-    if (name.value.length > 12) {
-      warning.long.classList.add('warning--show');
-      return;
-    }
-    if (name.value.length < 1) {
-      warning.short.classList.add('warning--show');
-      return;
-    }
-
-    // greeting.parentNode.removeChild(greeting);
-    ClientGame.init({ tagID: 'game', playerName: name.value });
-    greeting.remove();
-  }); */
 
   form.addEventListener('submit', submitName);
+
+  chatForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (chatInput.value) {
+      console.log('### chatInput.value: ', chatInput.value)
+
+      chatInput.value = '';
+    }
+
+
+  })
 });
